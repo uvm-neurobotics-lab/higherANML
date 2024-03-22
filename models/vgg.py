26ae7185a5d35a6e37d2a9ee82e3348424e04972
Neil Traft <ntraft@gmail.com>
2024-03-22 00:02:54 -0400

    Legacy version doesn't allow configuration of dropout. :-(

--- models/vgg.py
+++ models/vgg.py
@@ -23,62 +23,58 @@ class VGGEncoder(nn.Module):
 
 
 @register('vgg16')
-def vgg16(pretrained=False, progress=True, dropout=0.0, **kwargs):
+def vgg16(pretrained=False, progress=True, **kwargs):
     """
     VGG-16 model from Torchvision.
 
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet-1K.
         progress (bool): If True, displays a progress bar of the download to stderr.
-        dropout (float): The probability of DropOut on linear layers during training (0.0 means no DropOut).
         **kwargs: parameters passed to the ``torchvision.models.vgg.VGG`` base class. Please refer to the `source code
             <https://github.com/pytorch/vision/blob/main/torchvision/models/vgg.py>`_ for more details about this class.
     """
-    return torchvgg.vgg16(pretrained=pretrained, progress=progress, dropout=dropout, **kwargs)
+    return torchvgg.vgg16(pretrained=pretrained, progress=progress, **kwargs)
 
 
 @register('vgg16_bn')
-def vgg16_bn(pretrained=False, progress=True, dropout=0.0, **kwargs):
+def vgg16_bn(pretrained=False, progress=True, **kwargs):
     """
     VGG-16-BN model from Torchvision.
 
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet-1K.
         progress (bool): If True, displays a progress bar of the download to stderr.
-        dropout (float): The probability of DropOut on linear layers during training (0.0 means no DropOut).
         **kwargs: parameters passed to the ``torchvision.models.vgg.VGG`` base class. Please refer to the `source code
             <https://github.com/pytorch/vision/blob/main/torchvision/models/vgg.py>`_ for more details about this class.
     """
-    return torchvgg.vgg16_bn(pretrained=pretrained, progress=progress, dropout=dropout, **kwargs)
+    return torchvgg.vgg16_bn(pretrained=pretrained, progress=progress, **kwargs)
 
 
 @register('vgg16_encoder')
-def vgg16_encoder(pretrained=False, progress=True, dropout=0.0, **kwargs):
+def vgg16_encoder(pretrained=False, progress=True, **kwargs):
     """
     VGG-16 model from Torchvision, but only the encoder part (before the linear classifier).
 
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet-1K.
         progress (bool): If True, displays a progress bar of the download to stderr.
-        dropout (float): The probability of DropOut on linear layers during training (0.0 means no DropOut).
         **kwargs: parameters passed to the ``torchvision.models.vgg.VGG`` base class. Please refer to the `source code
             <https://github.com/pytorch/vision/blob/main/torchvision/models/vgg.py>`_ for more details about this class.
     """
-    vgg = torchvgg.vgg16(pretrained=pretrained, progress=progress, dropout=dropout, **kwargs)
+    vgg = torchvgg.vgg16(pretrained=pretrained, progress=progress, **kwargs)
     return VGGEncoder(vgg)
 
 
 @register('vgg16_bn_encoder')
-def vgg16_bn_encoder(pretrained=False, progress=True, dropout=0.0, **kwargs):
+def vgg16_bn_encoder(pretrained=False, progress=True, **kwargs):
     """
     VGG-16-BN model from Torchvision, but only the encoder part (before the linear classifier).
 
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet-1K.
         progress (bool): If True, displays a progress bar of the download to stderr.
-        dropout (float): The probability of DropOut on linear layers during training (0.0 means no DropOut).
         **kwargs: parameters passed to the ``torchvision.models.vgg.VGG`` base class. Please refer to the `source code
             <https://github.com/pytorch/vision/blob/main/torchvision/models/vgg.py>`_ for more details about this class.
     """
-    vgg = torchvgg.vgg16_bn(pretrained=pretrained, progress=progress, dropout=dropout, **kwargs)
+    vgg = torchvgg.vgg16_bn(pretrained=pretrained, progress=progress, **kwargs)
     return VGGEncoder(vgg)
